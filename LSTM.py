68dd5369454e3bbe78ae53afa70f52ebe726844a
harry2002731 <harry_2002@163.com>
2024-07-10 09:54:06 +0800

    增加数据集打乱

--- LSTM.py
+++ LSTM.py
@@ -5,19 +5,28 @@ from torch.utils.data import DataLoader
 import torch
 import torch.utils.data as data
 import torch.optim as optim
-import tqdm
 import matplotlib.pyplot as plt
 from scipy.interpolate import make_interp_spline
 from sklearn.preprocessing import MinMaxScaler
+import random
 
-batch_size = 128
+batch_size = 256
 input_size = 24
 hidden_size = 16
 hidden_size2 = 32
 num_layers = 3
 output_size = 1
-learning_rate = 0.001
-num_epochs = 1500
+num_epochs = 3000
+# 学习率
+learning_rate = 0.005
+# betas参数
+betas = (0.95, 0.999)
+# eps参数
+eps = 1e-8
+# 权重衰减
+weight_decay = 0
+# 是否使用AMSGrad
+amsgrad = True
 
 class Mydataset(data.Dataset):
     def __init__(self, x1,x2, y):
@@ -36,44 +45,55 @@ class Mydataset(data.Dataset):
 
     def __len__(self):
         return len(self.idx)
-                    
-def loadData(path,device,random_state):
+    
+def randomSortData(path,random_seed):
+    random.seed(random_seed)  
+    total_data_list = []
     with open(path, "r") as f:
         line = f.readline() # 读取第一行
-        data_label = []
-        real_data = []
-        cmd_data = []
         while line:
             data_list = line.split(" ")
             data_list.pop()
             data_list = [float(item) for item in data_list]
-            label = data_list[0]
-            len_data_list = len(data_list)
-            real_list = data_list[1:int((len_data_list-1)/2)+1]
-            cmd_list = data_list[int((len_data_list-1)/2)+1:]
-            while len(real_list) >input_size:
-                real_list.pop(0)
-                cmd_list.pop(0)
-            if len(real_list) == input_size:
-                data_label.append(label)
-                real_data.append(np.array(real_list))
-                cmd_data.append(np.array(cmd_list))
+            total_data_list.append(data_list)
             line = f.readline() # 读取下一行
+    random.shuffle(total_data_list)
+    return total_data_list
 
-        scaler = MinMaxScaler(feature_range=(-1, 1))
-        real_data = scaler.fit_transform(real_data)
+def loadData(path,device,random_state):
+    total_data_list = randomSortData(path,35)
+    data_label = []
+    real_data = []
+    cmd_data = []
+    for data_list in total_data_list:
 
-        scaler = MinMaxScaler(feature_range=(-1, 1))
-        cmd_data = scaler.fit_transform(cmd_data)
-        len_data = int(len(real_data)*(1 - random_state))
-        real_data = torch.tensor(real_data).to(device)
-        cmd_data = torch.tensor(cmd_data).to(device)
-        data_label = torch.tensor(np.array(data_label)).to(device)
+        data_list = [float(item) for item in data_list]
+        label = data_list[0]
+        len_data_list = len(data_list)
+        real_list = data_list[1:int((len_data_list-1)/2)+1]
+        cmd_list = data_list[int((len_data_list-1)/2)+1:]
+        while len(real_list) >input_size:
+            real_list.pop(0)
+            cmd_list.pop(0)
+        if len(real_list) == input_size:
+            data_label.append(label)
+            real_data.append(np.array(real_list))
+            cmd_data.append(np.array(cmd_list))
 
-        train_list = [real_data[0:len_data],cmd_data[0:len_data],data_label[0:len_data]]
-        test_list = [real_data[len_data:-1],cmd_data[len_data:-1],data_label[len_data:-1]]
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+    real_data = scaler.fit_transform(real_data)
 
-        return train_list,test_list
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+    cmd_data = scaler.fit_transform(cmd_data)
+    len_data = int(len(real_data)*(1 - random_state))
+    real_data = torch.tensor(real_data).to(device)
+    cmd_data = torch.tensor(cmd_data).to(device)
+    data_label = torch.tensor(np.array(data_label)).to(device)
+
+    train_list = [real_data[0:len_data],cmd_data[0:len_data],data_label[0:len_data]]
+    test_list = [real_data[len_data:-1],cmd_data[len_data:-1],data_label[len_data:-1]]
+
+    return train_list,test_list
 
 
 class LSTMModel(nn.Module):
@@ -104,19 +124,11 @@ class LSTMModel(nn.Module):
 class MultiInputLSTM(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, output_size):
         super(MultiInputLSTM, self).__init__()
-        # self.CMD_LSTM = nn.LSTM(8*11, 32, 5, batch_first=True,dropout=0.2)
-        # self.bn1 = nn.BatchNorm1d(32)
-
-        # self.Real_LSTM = nn.LSTM(8*11, 32, 5, batch_first=True,dropout=0.2)
-        # self.bn2 = nn.BatchNorm1d(32)
-
-
         self.CMD_LSTM = nn.LSTM(24, 64, 3, batch_first=True,dropout=0.2)
         self.bn1 = nn.BatchNorm1d(64)
 
         self.Real_LSTM = nn.LSTM(24, 64, 3, batch_first=True,dropout=0.2)
         self.bn2 = nn.BatchNorm1d(64)
-
 
         self.concat_LSTM = nn.LSTM(496, 128, 3, batch_first=True,dropout=0.6)
         self.bn3 = nn.BatchNorm1d(128)
@@ -125,34 +137,28 @@ class MultiInputLSTM(nn.Module):
         self.conv_layer2 = nn.Sequential(nn.Conv1d(in_channels=1, out_channels=8, kernel_size=6,padding=2))
 
         self.conv_layer3 = nn.Sequential(nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(2,1)))
-        self.max_pooling1 = nn.MaxPool1d(3, stride=2)
-        self.max_pooling2 = nn.MaxPool1d(3, stride=2)
-        self.relu1 =  nn.ReLU(inplace=True)
-        self.relu2 =  nn.ReLU(inplace=True)
+        self.max_pooling = nn.MaxPool1d(3, stride=2)
+        self.relu =  nn.ReLU(inplace=False)
 
         self.fc = nn.Linear(128, output_size)
 
     def forward(self, x1,x2):
         x1 = x1.view(-1,1,24)
         real_out, _ = self.Real_LSTM(x1)
-        real_out = self.bn1(real_out[:, -1, :]) 
+        real_out_bn = self.bn1(real_out[:, -1, :]) 
 
         x2 = x2.view(-1,1,24)
-        cmd_out, _ = self.CMD_LSTM(x2) # LSTM层
-        cmd_out = self.bn2(cmd_out[:, -1, :]) 
+        cmd_out, _ = self.CMD_LSTM(x2) 
+        cmd_out_bn = self.bn2(cmd_out[:, -1, :]) 
         
-        stack_data = torch.stack((real_out,cmd_out),dim=0).permute(1, 0, 2)
+        stack_data = torch.stack((real_out_bn,cmd_out_bn),dim=0).permute(1, 0, 2)
         stack_data = stack_data.view(-1,1,2,64)
-        stack_data = self.relu1(self.conv_layer3(stack_data))
+        stack_data = self.relu(self.conv_layer3(stack_data))
         stack_data = stack_data.view(-1,16,64)
-
-        out = self.max_pooling1(stack_data)    
+        out = self.max_pooling(stack_data)    
         out = out.view(-1,1,496)
 
-        # stack_data = (real_out + cmd_out)/2
-        # out = stack_data.view(-1, 1, 32)
         out,_ = self.concat_LSTM(out)
-
         out = self.fc(out[:, -1, :]) # 全连接层
         return out
 
@@ -161,27 +167,17 @@ def train(device):
     test_data, test_data_2 ,test_labels = test_
     train_data, train_data_2 ,train_labels = train_
     dataset = Mydataset(train_data,train_data_2, train_labels)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False) 
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
 
-    # test_data, test_data_2 ,test_labels = loadData(r"data\test.txt",device,0.2)
     test_dataset = Mydataset(test_data,test_data_2, test_labels)
     test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+
+    # 定义损失函数和优化器
+    loss_function = nn.MSELoss()
 
     # 创建模型实例
     model = MultiInputLSTM(input_size, hidden_size, num_layers, output_size).to(device)
 
-    # 定义损失函数和优化器
-    loss_function = nn.MSELoss()
-    # 学习率
-    learning_rate = 0.005
-    # betas参数
-    betas = (0.95, 0.999)
-    # eps参数
-    eps = 1e-8
-    # 权重衰减
-    weight_decay = 0
-    # 是否使用AMSGrad
-    amsgrad = True
     # 创建Adam优化器
     optimizer = optim.Adam(
         model.parameters(),
@@ -191,20 +187,19 @@ def train(device):
         weight_decay=weight_decay,
         amsgrad=amsgrad
     )
-
     iter = 0
     print('TRAINING STARTED.\n')
     for epoch in range(num_epochs):
-        for i, (images, train_labels) in enumerate(dataloader):
+        for i, (motor_data, train_labels) in enumerate(dataloader):
             # pass
             model.train()
-            imgs1 = images[0]                
-            imgs2 = images[1]                
-            images = imgs1.view(-1, 1, input_size)
-            images2 = imgs2.view(-1, 1, input_size)
+            real_motor = motor_data[0]                
+            cmd_motor = motor_data[1]                
+            real_motor = real_motor.view(-1, 1, input_size)
+            cmd_motor = cmd_motor.view(-1, 1, input_size)
             model = model.to(device, dtype=torch.float64)
 
-            outputs = model(images,images2)
+            outputs = model(real_motor,cmd_motor)
             train_labels = train_labels.unsqueeze(1)  # 将目标的形状从[2]变为[2, 1]
             outputs *= 100
             train_labels *= 100
@@ -219,20 +214,15 @@ def train(device):
                 model.eval()
                 loss_ = []
                 for i, (images, test_labels) in enumerate(test_loader):
-                    imgs1 = images[0]                
-                    imgs2 = images[1]                
-                    images = imgs1.view(-1, 1, input_size)
-                    images2 = imgs2.view(-1, 1, input_size)
+                    test_real_motor = images[0].view(-1, 1, input_size)                
+                    test_cmd_motor = images[1].view(-1, 1, input_size)                
 
-                    outputs = model(images,images2)
+                    outputs = model(test_real_motor,test_cmd_motor)
                     test_labels = test_labels.unsqueeze(1)  # 将目标的形状从[2]变为[2, 1]
                     outputs *= 100
                     test_labels *= 100
                     loss_.append(loss_function(outputs, test_labels).cpu().detach().numpy())
                 avg_ = np.mean(np.array(loss_))
-                # print(f'Iteration: {epoch}\t Loss: {avg_.item():.4f}')
-                # Calculate Loss
-                # print(f'Epoch: {epoch + 1}/{num_epochs}\t Iteration: {iter}\t Loss: {loss.item():.4f}') 
                 print(f'Epoch: {epoch + 1}/{num_epochs}\t Iteration: {iter}\t Loss: {loss.item():.4f} test_Loss: {avg_.item():.4f}') 
                 
     torch.save(model, r"model.pth")
@@ -241,7 +231,7 @@ def test(device):
     loss_function = nn.MSELoss()
 
     model = torch.load(r"model.pth").to(device)
-    train_, test_ = loadData(r"data\train.txt",device,0.8)
+    train_, test_ = loadData(r"data\train.txt",device,0.2)
     test_data, test_data_2 ,test_labels = test_
 
     dataset = Mydataset(test_data,test_data_2, test_labels)
@@ -254,7 +244,7 @@ def test(device):
         pred = []
         y = []
         loss_list = []
-
+        i = 0
         # Iterate through test dataset
         for i, (images, test_labels) in enumerate(test_loader):
             imgs1 = images[0]                
@@ -289,8 +279,7 @@ def test(device):
         plt.show()
 if __name__== "__main__":
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
     # print(torch.cuda.is_available())
-    # train(device)
+    train(device)
     test(device)
 
