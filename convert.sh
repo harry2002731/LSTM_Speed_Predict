3e8e4c1eed3435a2255af3a800c6d77da2105b37
HarryWen <harry_2002@163.com>
2024-08-07 09:05:57 +0800

    添加ONNX转MNN的转换脚本

---
+++ convert.sh
@@ -0,0 +1,1 @@
+/home/ubuntu/Desktop/MNN/build/MNNConvert -f ONNX --modelFile  '/home/ubuntu/Desktop/project/LSTM_Speed_Predict/model/model.onnx' --MNNModel '/home/ubuntu/Desktop/project/LSTM_Speed_Predict/model/model.mnn' --bizCode biz
